ec78367133270470bd623c284ef300df8693f6c1
João Dinis Ferreira <hello@joaof.eu>
2020-12-06 16:16:19 +0100

    Solve day 2

---
+++ 2020/day-02-part1.py
@@ -0,0 +1,35 @@
+import os
+import pathlib
+
+from parse import parse
+
+
+def parse_input(raw_input):
+    def parse_line(line):
+        p = parse("{}-{} {}: {}", line)
+
+        return [int(p[0]), int(p[1]), p[2], p[3]]
+
+    return [parse_line(line) for line in raw_input]
+
+
+def solve(puzzle_input):
+    def solve_line(line):
+        return line[0] <= line[3].count(line[2]) <= line[1]
+
+    print(sum([solve_line(line) for line in puzzle_input]))
+
+
+def main():
+    path = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
+
+    with (path / 'day-02.input').open() as f:
+        puzzle_input = f.readlines()
+
+    puzzle_input = parse_input(puzzle_input)
+
+    solve(puzzle_input)
+
+
+if __name__ == "__main__":
+    main()
